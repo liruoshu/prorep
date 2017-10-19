926b1efb23d3fe7b01b88ff1e10b0c4367829af0
liruoshu <chenmuxiaoxiao@163.com>
2017-10-19 15:33:45 +0800

    second

---
+++ src/com/company/Hello.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Hello {
+    public static void main(String[] args) {
+        for(int i=0;i>100;i++){
+        System.out.println(18);}
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("11111");
+
+        ArrayList<Integer> a= new ArrayList<Integer>();
+        a.add(100);
+
     }
 }
